eb947f89837ee1b458361a10aac0e4e391a10423
Greg Price <gnprice@gmail.com>
2017-07-24 18:15:07 -0700

    realm filters: Put the custom error-data logic into a class.
    This is the last user of the `data` parameter to `json_error`,
    allowing us to simplify that API before refactoring it.

--- zerver/views/realm_filters.py
+++ zerver/views/realm_filters.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import
 
-from typing import Text
+from typing import Dict, List, Text
 from django.core.exceptions import ValidationError
 from django.http import HttpRequest, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
@@ -8,6 +8,7 @@ from django.utils.translation import ugettext as _
 
 from zerver.decorator import has_request_variables, REQ, require_realm_admin
 from zerver.lib.actions import do_add_realm_filter, do_remove_realm_filter
+from zerver.lib.exceptions import JsonableError
 from zerver.lib.response import json_success, json_error
 from zerver.lib.rest import rest_dispatch as _rest_dispatch
 from zerver.lib.validator import check_string
@@ -19,6 +20,22 @@ def list_filters(request, user_profile):
     # type: (HttpRequest, UserProfile) -> HttpResponse
     filters = realm_filters_for_realm(user_profile.realm_id)
     return json_success({'filters': filters})
+
+
+class RealmFilterValidationError(JsonableError):
+    data_fields = ['errors']
+
+    def __init__(self, err):
+        # type: (ValidationError) -> None
+        self.errors = err.message_dict  # type: Dict[str, List[Text]]
+
+        # This message is a bit of a hack -- whatever error happens to show up
+        # first in traversing the dict, if there are multiple errors stored at
+        # different keys.  The "return all errors" logic here would be good to
+        # generalize and use in other places where we handle ValidationError,
+        # but the message should be cleaned up when doing so.
+        all_messages = sum(self.errors.values(), [])  # type: List[Text]
+        self._msg = all_messages[0]  # type: Text
 
 
 @require_realm_admin
@@ -34,7 +51,7 @@ def create_filter(request, user_profile, pattern=REQ(),
         )
         return json_success({'id': filter_id})
     except ValidationError as e:
-        return json_error(e.messages[0], data={"errors": dict(e)})
+        raise RealmFilterValidationError(e)
 
 
 @require_realm_admin
